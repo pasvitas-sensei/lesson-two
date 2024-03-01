808d1ffb3a9b2dd52ef2471c45fdfa78b2c6c0a0
Vitaly Pashichev <pasvitas1@gmail.com>
2024-03-01 23:03:03 +0300

    Start project

---
+++ src/main/java/ru/pasvitas/teaching/lessontwo/LessonTwoApplication.java
@@ -0,0 +1,13 @@
+package ru.pasvitas.teaching.lessontwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LessonTwoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/pasvitas/teaching/lessontwo/LessonTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pasvitas.teaching.lessontwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
